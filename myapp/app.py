d8649422c876c117cc7536a000b9cbfe3d3a695c
strikade <andtensai@hotmail.com>
2024-05-28 16:27:41 -0500

    Update app.py

--- myapp/app.py
+++ myapp/app.py
@@ -6,7 +6,15 @@ def hello_world():
   return {
     'message': 'hola, Mundo!!!'
   }
+"""correr un comando en lenguaje python que permita listar objetos
+  en /storage/<uid>
 
+  incluir /public/ que permite listar los archivos del contenedor <uid>
+
+  incluir /download/<name.ext> que permite descargar el archivo <name.ext>
+
+
+  /upload/<uid>/<name.ext> que permite enviar un archivo al contenedor <uid>
 @app.route('/despedirse')
 def bye_world():
   return {
